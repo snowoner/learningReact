5467e48b916ad0c3478cd36ff176c91480090d6f
oscar urgelles <oscar.urgelles@gmail.com>
2019-06-02 16:26:46 +0200

    changes

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import './Contact.css';
+
+
+class Contact extends Component {
+
+  render() {
+    return (
+      <div className="Contact">
+        <h2>Contact</h2>
+      </div>
+    )
+  }
+
+}
+
+export default Contact
